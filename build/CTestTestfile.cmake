c64941d9de6ba5df87690fc5898ad93f78c84bf5
Seba485 <sebastiano.toniolo@studenti.unipd.it>
2024-07-04 16:06:37 +0200

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sebastiano/robot_sim/src
+# Build directory: /home/sebastiano/robot_sim/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_model_pkg")
+subdirs("wheelchair_iaslab_odometry")
